8aa3dea92b8364e9019b6d1413fda401b19c1bbb
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-04-14 10:09:24 +0000

    Added variables at script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Set Up Variable
+let txt = "";
+
+/*
+    Function to Return the Greatest Product of four adjacent numbers
+    (Vertically, Horizontally or Diagonally)
+*/
+function largestGridProduct(arr) {
+    let maxProduct = 0;
+    let currProduct = 0;
+    function maxProductChecker(n) {
+        if (n>maxProduct) return maxProduct = n;
+    }
+
+    // Loop Rows
+    
+}
